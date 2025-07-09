61fcbf3565924839a9d98c5813dfbaac8aeae622
dani <danimoraga1997@gmail.com>
2025-07-09 11:58:10 +0200

    Add dynamic styles to checkboxes

--- scripts/style.js
+++ scripts/style.js
@@ -13,3 +13,12 @@ function setSliderStyle() {
     slider.style.background = 
         `linear-gradient(to right, #A4FFAF ${valPercent}%, #18171F ${valPercent}%)`;
 }
+
+// Checkboxes
+const checkboxes = document.querySelectorAll('.generate__options__item__checkbox');
+checkboxes.forEach(handleCheckbox);
+function handleCheckbox(checkbox) {
+    checkbox.addEventListener('click', () => {
+        checkbox.classList.toggle('checkbox--active');
+    })
+} 
